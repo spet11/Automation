f3974498c05ceb6b356e0f84b91be17a31f64cac
sanjay patel <nishs1811@gmail.com>
2022-04-12 19:58:11 +0100

    file add

---
+++ Operator.java
@@ -0,0 +1,35 @@
+package com.Automation;
+
+public class Operator {
+    public static void main(String[] args) {
+        int a =1;
+        System.out.println(a++);
+        System.out.println(a++);
+        System.out.println(a++);
+        System.out.println(a++);
+        System.out.println(a++);
+        System.out.println(a++);
+        System.out.println(a++);
+        System.out.println(a++);
+        System.out.println(a++);
+        System.out.println(a++);
+
+        // Decrement Operator
+        System.out.println(" Decrement Operator" );
+        System.out.println(a--);
+        System.out.println(a--);
+        System.out.println(a--);
+        System.out.println(a--);
+        System.out.println(a--);
+        System.out.println(a--);
+        System.out.println(a--);
+        System.out.println(a--);
+        System.out.println(a--);
+        System.out.println(a--);
+        System.out.println(a--);
+
+    }
+}
+
+
+
